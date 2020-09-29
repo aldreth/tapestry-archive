c0372cf508e5bd4da20374ff339081477613cbbb
Edward Andrews-Hodgson <edward@aldreth.com>
2020-09-29 19:06:19 +0100

    Get videos & add rubocop

--- Gemfile
+++ Gemfile
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 source 'https://rubygems.org'
 
 gem 'dotenv'
 gem 'httparty'
 gem 'mini_exiftool_vendored'
 gem 'nokogiri'
+
+gem 'rubocop', require: false
--- main.rb
+++ main.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 require 'dotenv/load'
 require 'httparty'
 require 'mini_exiftool_vendored'
 require 'nokogiri'
 
-def getDoc(observation_id)
+def get_doc(observation_id)
   image_dir = './images/'
 
   cookie_name = 'tapestry_session'
   cookie_value = ENV['COOKIE_VALUE']
 
-  options = { 'headers': { 'Cookie': "#{cookie_name}=#{cookie_value}" }}
+  options = { 'headers': { 'Cookie': "#{cookie_name}=#{cookie_value}" } }
 
   base_url = 'https://tapestryjournal.com/s/scarcroft-green-nursery/observation'
 
@@ -20,9 +22,17 @@ def getDoc(observation_id)
   Nokogiri::HTML(raw)
 end
 
-  def getImagesForPage(doc)
-  image_dir = './images/'
+def get_file_name(title:, index:, video: false)
+  file_name = './images/'
+  file_name += title.downcase.gsub(' ', '-')
+  file_name += "-#{index}" if index.positive?
+  file_name += video ? '.mp4' : '.jpeg'
+  file_name
+end
+
+def get_images_for_page(doc)
   images = doc.css('.obs-media-gallery-main img')
+  videos = doc.css('.obs-media-gallery-main .obs-video-wrapper video source')
 
   title = doc.css('h1').first.text.strip
   image_description = doc.css('.page-note p').text.strip.gsub(/\s+/, ' ')
@@ -31,32 +41,43 @@ end
     image_url = img.attribute('src').value
     image = HTTParty.get(image_url)
 
-    file_name = image_dir
-    file_name += title.downcase.gsub(' ', '-')
-    file_name += "-#{idx}" if idx > 0
-    file_name += '.jpeg'
-
+    file_name = get_file_name(title: title, index: idx)
     File.write(file_name, image)
 
-    photo =  MiniExiftool.new(file_name)
+    photo = MiniExiftool.new(file_name)
     photo.title = title
     photo.image_description = image_description
     photo.save
   end
+
+  videos.each_with_index do |img, idx|
+    video_url = img.attribute('src').value
+    video = HTTParty.get(video_url)
+
+    file_name = get_file_name(title: title, index: idx, video: true)
+    File.write(file_name, video)
+
+    # photo =  MiniExiftool.new(file_name)
+    # photo.title = title
+    # photo.image_description = image_description
+    # photo.save
+  end
 end
 
-def getNextObservationId(doc)
+def get_next_observation_id(doc)
   next_link = doc.css('li.next a')
   return nil if next_link.nil? || next_link.empty?
-  next_link.attribute('href').value.match(/https:\/\/tapestryjournal.com\/s\/scarcroft-green-nursery\/observation\/(\d*)/)[1]
-end
 
+  next_link.attribute('href')
+           .value
+           .match(/https:\/\/tapestryjournal.com\/s\/scarcroft-green-nursery\/observation\/(\d*)/)[1]
+end
 
 observation_id = ENV['FIRST_OBSERVATION_ID']
 
 while observation_id do
   puts observation_id
-  doc = getDoc(observation_id)
-  getImagesForPage(doc)
-  observation_id = getNextObservationId(doc)
+  doc = get_doc(observation_id)
+  get_images_for_page(doc)
+  observation_id = get_next_observation_id(doc)
 end
